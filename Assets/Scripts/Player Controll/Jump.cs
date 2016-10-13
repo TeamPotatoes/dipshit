5cbe981ae3a4cdb8123daab9054af348dfdc67d3
VALENTIN\valen <valentinbelotserkovskiy@gmail.com>
2016-10-13 14:14:59 +0300

    Level prototype
    Player movement and jumping scripts

---
+++ Assets/Scripts/Player Controll/Jump.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jump : MonoBehaviour {
+    private bool grounded = true;
+    public float JumpPower = 200;
+    private bool hasJump = false;
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (!grounded && rb.velocity.y == 0)
+        {
+            grounded = true;
+        }
+        if (Input.GetButtonDown("Jump") && grounded == true)
+        {
+            hasJump = true;
+        }
+        
+	}
+    void FixedUpdate()
+    {
+        if (hasJump)
+        {
+            rb.AddForce(transform.up * JumpPower);
+            grounded = false;
+            hasJump = false;
+        }
+    }
+}
