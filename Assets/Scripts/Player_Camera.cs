86d3bbf9d9848d98d10c9b333ab12a9301511e15
VALENTIN\valen <valentinbelotserkovskiy@gmail.com>
2016-10-20 23:55:27 +0300

    Small Update

--- Assets/Scripts/Player_Camera.cs
+++ Assets/Scripts/Player_Camera.cs
@@ -5,17 +5,22 @@ public class Player_Camera : MonoBehaviour {
 
     public GameObject Player;
     private Vector3 offset;
+    private Vector3  Position;
+    //public Transform target;
 
 
 	// Use this for initialization
 	void Start ()
     {
+       // Position = transform.TransformPoint(Vector3.right * 2);
+       // Instantiate(Player, Position, Player.transform.rotation);
         offset = transform.position - Player.transform.position;
     }
 	
 	// Update is called once per frame
 	void LateUpdate ()
     {
-        transform.position = Player.transform.position + offset;
+        //transform.LookAt(target);
+       transform.position = Player.transform.position + offset;
     }
 }
