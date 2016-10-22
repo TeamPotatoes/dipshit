c3b372845fa09f0270b8f899437a8657a10d7cc6
Gaio26 <gismo0_o@mail.ru>
2016-10-22 15:23:15 +0300

    Small camera changes

--- Assets/Scripts/Player_Camera.cs
+++ Assets/Scripts/Player_Camera.cs
@@ -5,21 +5,17 @@ using System.Collections;
 public class Player_Camera : MonoBehaviour
 {
 
-
-
     public GameObject Player;
-   // private float speedMod = 10.0f;
-    private Vector3 offset;
+    private float turn_speed = 4.0f;
+    private Vector3 offsetX;
+    private Vector3 offsetY;
     public Transform target;
-
 
      // Use this for initialization
      void Start()
      {
-
-        
-        offset = Player.transform.position ;// - Player.transform.position;
-
+     offsetX = new Vector3(target.position.x, target.position.y + 10.0f, target.position.z + 5.0f);
+     offsetY = new Vector3(target.position.x, target.position.y, target.position.z);
     }
     void Update()
     {
@@ -29,6 +25,9 @@ public class Player_Camera : MonoBehaviour
     // Update is called once per frame
     void LateUpdate ()
     {
+        offsetX = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * turn_speed, Vector3.up) * offsetX;
+        offsetY = Quaternion.AngleAxis(Input.GetAxis("Mouse Y") * turn_speed, Vector3.right) * offsetY;
+        transform.position = target.position + offsetX + offsetY;
         transform.LookAt(target);
        // transform.RotateAround(offset, Vector3.up, 20 * Time.deltaTime);
 
