f5329cabffd077a99c1514383e7595f0751a62a6
Gaio26 <gismo0_o@mail.ru>
2016-10-22 12:17:15 +0300

    Anim changes
    123

--- Assets/Scripts/Player_animation.cs
+++ Assets/Scripts/Player_animation.cs
@@ -7,12 +7,13 @@ public class Player_animation : MonoBehaviour {
     private AudioSource source;
     private float inputSides;
     private float inputFronts;
+    //private float inputTurns;
 
     // Use this for initialization
     void Start () {
         anim = GetComponent<Animator>();
         source = GetComponent<AudioSource>();
-        //this.animation["running"].wrapMode = WrapMode.PingPong;
+        
     }
     
 	// Update is called once per frame
@@ -22,11 +23,7 @@ public class Player_animation : MonoBehaviour {
             anim.Play("jump_stay", -1, 0f);
             source.PlayOneShot(jump, 1.0f);
         }
-        //if (Input.GetKey(KeyCode.W))
-        //{
-        //    anim.Play("running", -1, 0f);
-        //}
-        if (Input.GetButtonDown("Horizontal") && Input.GetButtonDown("Jump") && GetComponent<Jump>().grounded == true)
+       if (Input.GetKey(KeyCode.Space) && Input.GetAxis("Vertical")!=0 &&  GetComponent<Jump>().grounded == true)
         {
             anim.Play("running_jump", -1, 0f);
             source.PlayOneShot(jump, 1.0f);
@@ -34,7 +31,10 @@ public class Player_animation : MonoBehaviour {
 
         inputSides = Input.GetAxis("Horizontal");
         inputFronts = Input.GetAxis("Vertical");
+        //inputTurns = transform.rotation.x; 
         anim.SetFloat("inputSides", inputSides);
         anim.SetFloat("inputFronts", inputFronts);
+        //anim.SetFloat("inptTurns", inputTurns);
+        anim.SetBool("run", GetComponent<Movement>().run);
     }
 }
