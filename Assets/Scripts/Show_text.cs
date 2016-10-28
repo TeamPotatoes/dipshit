bf7cd399ea024049d0f5c5ccf9db26086409a397
VALENTIN\valen <valentinbelotserkovskiy@gmail.com>
2016-10-28 17:34:10 +0300

    HP bar Script

--- Assets/Scripts/Show_text.cs
+++ Assets/Scripts/Show_text.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 //СКРИПТ ОТОБРАЖАЕТ ВСПЛЫВАЮЩИЕ ТЕКСТЫ ЕСЛИ К НИМ БЛИЗКО ПОДОЙТИ
 public class Show_text : MonoBehaviour {
        
-    public GameObject[] ShowMe; //Эта штука в инспекторе (в скрипте) при старте игры, отобразит все объекты с тэгом PopUp. Кусочек кода для работы есть в Update.
+    public GameObject[] ShowMe;
 
     void Start()
     {
@@ -14,17 +14,17 @@ public class Show_text : MonoBehaviour {
 
     void LateUpdate()
     {
-        ShowMe = GameObject.FindGameObjectsWithTag("PopUp"); // тот самый кусочек кода
+        ShowMe = GameObject.FindGameObjectsWithTag("PopUp"); 
         
-            GameObject[] signs; // оказывается можно создавать переменные и сразу придавать им значение непосредственно в скрипте. Эта переменная - список
-            signs = GameObject.FindGameObjectsWithTag("PopUp"); // оказывается есть FindGameObjectWithTag и FindGameObjectsWithTag - первое переменная, второе список
-            GameObject closest = null; //объявляем пустую переменную - ближайший объект 
-            float distance = Mathf.Infinity; //объявляем расстояние - математическая бесконечность (т.е. все)
-            foreach (GameObject sign in signs) // создаем объект sign. для каждого (foreach) sign в списке signs - делаем следующее ... 
+            GameObject[] signs; 
+            signs = GameObject.FindGameObjectsWithTag("PopUp"); 
+            GameObject closest = null; 
+            float distance = Mathf.Infinity; 
+            foreach (GameObject sign in signs) 
             {
-                Vector3 diff = sign.transform.position - transform.position; // вычисляем расстояние
-                float curdistance = diff.sqrMagnitude; //делаем переменную с текущим расстоянием и приравниваем его к вычесленному квадрату расстояния...ипаная математика 
-                if (curdistance < distance) // тут понятно
+                Vector3 diff = sign.transform.position - transform.position; 
+                float curdistance = diff.sqrMagnitude;
+                if (curdistance < distance) 
                 {
                     closest = sign; 
                     distance = curdistance; 
